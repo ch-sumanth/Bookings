cc740bb0f30e53452213ba7180537d31cb9df36f
Sumanth <sumanthchunduri123@gmail.com>
2022-09-10 16:10:45 +0530

    Re-opened

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -25,17 +25,10 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIp := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "hello from about route"
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["ip"] = remoteIP
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{})
 }
